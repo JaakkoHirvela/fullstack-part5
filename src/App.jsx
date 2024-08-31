c2c9230d67b67fba116ec0370c0312bd3706acc6
Jaakko Hirvelä <jaakko.hirvela@tuni.fi>
2024-08-31 19:44:42 +0300

    5.1: add login functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,58 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    console.log("logging in with", username, password);
+
+    try {
+      const loginData = await loginService.login({ username, password });
+      setUser(loginData);
+      setUsername("");
+      setPassword("");
+    } catch (error) {
+      console.error("error logging in:", error);
+    }
+  };
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            username <input type="text" value={username} name="Username" onChange={({ target }) => setUsername(target.value)} />
+          </div>
+          <div>
+            password <input type="password" value={password} name="Password" onChange={({ target }) => setPassword(target.value)} />
+          </div>
+          <button type="submit">login</button>
+        </form>
+      </div>
+    );
+  }
 
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
+      <div style={{ paddingBottom: "10px" }}>Logged in as {user.name}</div>
+      {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
-      )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
