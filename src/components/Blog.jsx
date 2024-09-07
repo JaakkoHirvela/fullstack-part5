6347a1994ad59f7b5e82dbbc1a43f91c22e30ebd
Jaakko Hirvelä <jaakko.hirvela@tuni.fi>
2024-09-07 16:39:56 +0300

    5.7: hide/show extra info for Blog

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,41 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+  const [visible, setVisible] = useState(false);
+
+  const hideWhenVisible = { display: visible ? "none" : "" };
+  const showWhenVisible = { display: visible ? "" : "none" };
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility} style={hideWhenVisible}>
+          view
+        </button>
+        <button onClick={toggleVisibility} style={showWhenVisible}>
+          hide
+        </button>
+      </div>
+      <div style={showWhenVisible}>
+        <p>{blog.url}</p>
+        <p>
+          likes {blog.likes} <button>like</button>
+        </p>
+        <p>{blog.user.name}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
